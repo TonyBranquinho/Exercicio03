4c85c42a37c62f018332dc9bb2c902d617d5c15e
Tony <antony.branquinho@gmail.com>
2023-12-28 10:01:51 -0300

    basico

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro valores.");
+            int primeiro = int.Parse(Console.ReadLine());
+            int segundo = int.Parse(Console.ReadLine());
+            int terceiro = int.Parse(Console.ReadLine());
+            int quarto = int.Parse(Console.ReadLine());
+
+            int diferenca = primeiro * segundo - terceiro * quarto;
+            Console.WriteLine("DIFERENCA = " + diferenca);
+        }
+    }
+}
